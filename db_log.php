6389869e721f4049cc8c225f7f35dc277196bd64
Alishokh <alishotaigunshoev@mail.ru>
2021-12-23 19:42:47 +0100

    new php file

---
+++ db_log.php
@@ -0,0 +1,14 @@
+<?php
+    /* created a new var and assigned them from our log in.php */
+    $email =$_POST['projemail'];
+    $pass =$_POST['projpass'];
+    
+    /*connection between our var and database */
+    $mysql = new mysqli('localhost','root','','db_forlogin');
+    $mysql ->query("INSERT INTO `users`(`email`, `password`)
+    VALUES('$email','$pass')");
+    $mysql ->close();
+    
+    /*going beack to our web page */
+    header('Location:/forproject/Project_wsiz.php');
+?>
